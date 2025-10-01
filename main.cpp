a1a0e9a55b4f4220e0b8f7ee8a159703a132fc7e
Gorgegrouse <gorgegrouse@gmail.com>
2025-09-30 20:00:44 -0400

    adding libgpiod stuff

--- main.cpp
+++ main.cpp
@@ -27,17 +27,29 @@ import SensorModule;
     constexpr unsigned number_of_pins_on_raspberry_pi = 40u;
 
     //initialize sensors
-    
 
+    constexpr auto PinsForAccelerometer = {3,5};
+    //Sda 2
+    //clock 3
+    enum AccelerometerPins {
+        SDA = 2,
+        CLOCK = 3,
+        OUTPUT_1 = 6,
+        OUTPUT_2 = 4
+    };
+    //accelerometer
+    pinMode(CLOCK, GPIO_CLOCK);
+    pinMode(SDA, INPUT);
+    pinMode(OUTPUT_1, OUTPUT);
+    pinMode(OUTPUT_2, OUTPUT);
     //process sensors in parallel
     {
         using std::jthread;
         using flight_computer::sensor_processor;
         // jthread accelerator_processor(sensor_processor,  );
-
+    
         
-    }
-
+    }   
     std::array<int, number_of_pins_on_raspberry_pi> pin_mapping{};
     //main loop for running the 
     while (true) {
